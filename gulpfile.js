3ffe935208f1fe078752a7ec4ba4cff43dc10d34
Yura <stauber.j@mail.ru>
2025-01-01 18:48:56 +0300

    shopping cart layout

--- gulpfile.js
+++ gulpfile.js
@@ -20,9 +20,10 @@ gulp.task("styles", function () {
   return gulp
     .src("src/sass/**/*.+(scss|sass)")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(rename({ suffix: ".min", prefix: "" }))
     .pipe(autoprefixer())
-    .pipe(cleanCSS({ compatibility: "ie8" }))
+    // .pipe(cleanCSS({ compatibility: "ie8" })) // сжатие css
     .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
@@ -39,7 +40,7 @@ gulp.task("watch", function () {
 gulp.task("html", function () {
   return gulp
     .src("src/*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    // .pipe(htmlmin({ collapseWhitespace: true })) // сжатие html
     .pipe(gulp.dest("dist/"));
 });
 
